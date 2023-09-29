d2a6897b8376dac29f470ddd21f4bb6d2fb79826
Drew <dcm752052@gmail.com>
2023-09-28 22:11:46 -0400

    Created Deck.java.

---
+++ src/main/java/com/drewm/model/Deck.java
@@ -0,0 +1,35 @@
+package com.drewm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Deck {
+    @Id
+    @SequenceGenerator(name="deck_id_sequence", sequenceName = "deck_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "deck_id_sequence")
+    private Integer id;
+    @NonNull
+    private Integer userId;
+    @NonNull
+    private String name;
+    private boolean isPrivate;
+    private LocalDateTime createdAt;
+
+    public Deck(@NonNull Integer userId, @NonNull String name, boolean isPrivate) {
+        this.userId = userId;
+        this.name = name;
+        this.isPrivate = isPrivate;
+        this.createdAt = LocalDateTime.now();
+    }
+}
