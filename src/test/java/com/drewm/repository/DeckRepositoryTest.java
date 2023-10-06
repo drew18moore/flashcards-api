4f8be8f3b54732b1bf5684172a88d0d03f83724a
Drew <dcm752052@gmail.com>
2023-10-05 21:14:56 -0400

    Created test for DeckRepository class.

---
+++ src/test/java/com/drewm/repository/DeckRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.drewm.repository;
+
+import com.drewm.model.Deck;
+import com.drewm.model.User;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@RequiredArgsConstructor
+class DeckRepositoryTest {
+    @Autowired
+    private DeckRepository deckRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.save(
+                new User(1, "test user",  "testuser", "pass123")
+        );
+    }
+
+    @AfterEach
+    void tearDown() {
+        deckRepository.deleteAll();
+    }
+
+    @Test
+    void findAllByUserId() {
+        Deck deck = Deck.builder()
+                .userId(1)
+                .name("Deck #1")
+                .isPrivate(true)
+                .build();
+        deckRepository.save(deck);
+
+        List<Deck> decks = deckRepository.findAllByUserId(1);
+        assertThat(decks.size()).isEqualTo(1);
+        assertThat(decks.get(0)).isEqualTo(deck);
+    }
+
+}
