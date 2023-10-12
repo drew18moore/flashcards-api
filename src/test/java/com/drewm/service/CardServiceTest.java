59feb5546a83edf6401b306d3d5e40d5c2098b93
Drew <dcm752052@gmail.com>
2023-10-11 20:02:30 -0400

    Created test for newCard method.

---
+++ src/test/java/com/drewm/service/CardServiceTest.java
@@ -0,0 +1,71 @@
+package com.drewm.service;
+
+import com.drewm.dto.CardDTO;
+import com.drewm.dto.NewCardRequest;
+import com.drewm.model.Card;
+import com.drewm.model.Deck;
+import com.drewm.model.User;
+import com.drewm.repository.CardRepository;
+import com.drewm.repository.DeckRepository;
+import com.drewm.utils.CardDTOMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CardServiceTest {
+    @Mock
+    private CardRepository cardRepository;
+
+    @Mock
+    private CardDTOMapper cardDTOMapper;
+
+    @Mock
+    private DeckRepository deckRepository;
+
+    private CardService cardService;
+
+    @BeforeEach
+    void setUp() {
+        cardService = new CardService(cardRepository, cardDTOMapper, deckRepository);
+    }
+
+    @Test
+    void createNewCardInDeck() {
+        // given
+        User user = new User(1, "test user", "testuser", "pass123");
+        Authentication authentication = mock(Authentication.class);
+        NewCardRequest request = new NewCardRequest(1, "Front", "Back");
+        Deck deck = new Deck(request.deckId(), 1, "Deck #1", true, null);
+        Card card = new Card(user.getId(), request.deckId(), request.frontText(), request.backText());
+        CardDTO mockCardDTO = new CardDTO(card.getId(), user.getId(), deck.getId(), card.getFrontText(), card.getBackText(), null);
+
+        // when
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(deckRepository.findById(deck.getId())).thenReturn(Optional.of(deck));
+        when(cardRepository.save(any())).thenReturn(card);
+        when(cardDTOMapper.apply(any(Card.class))).thenReturn(mockCardDTO);
+
+        // then
+        CardDTO cardDTO = cardService.createNewCardInDeck(request, authentication);
+
+        // assert
+        verify(cardRepository, times(1)).save(any());
+        assertThat(request.deckId()).isEqualTo(cardDTO.deckId());
+        assertThat(request.frontText()).isEqualTo(cardDTO.fontText());
+        assertThat(request.backText()).isEqualTo(cardDTO.backText());
+    }
+
+    @Test
+    void editCard() {
+    }
+}
