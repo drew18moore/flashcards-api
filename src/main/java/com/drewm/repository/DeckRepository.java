76a636d633a5f5238c08b5ef84e42d4dcc1d627a
Drew <dcm752052@gmail.com>
2023-12-07 23:45:46 -0500

    Fixed custom deck search query. Now queries current user's decks that match query instead of all user's decks.

--- src/main/java/com/drewm/repository/DeckRepository.java
+++ src/main/java/com/drewm/repository/DeckRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 public interface DeckRepository extends JpaRepository<Deck, Integer> {
     List<Deck> findAllByUserId(Integer userId);
 
-    @Query(value = "SELECT * FROM Deck d WHERE (LOWER(d.name) LIKE LOWER(CONCAT('%', :query, '%')) AND d.is_private = false) OR d.user_id = :userId", nativeQuery = true)
+    @Query(value = "SELECT * FROM Deck d WHERE (LOWER(d.name) LIKE LOWER(CONCAT('%', :query, '%')) AND d.is_private = false) OR (LOWER(d.name) LIKE LOWER(CONCAT('%', :query, '%')) AND d.user_id = :userId)", nativeQuery = true)
     List<Deck> searchDecks(String query, Integer userId);
 }
